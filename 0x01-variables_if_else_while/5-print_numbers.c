3dc03e19a47cf9608e74224f6db224a70d792170
mahdixabid <yuri@instance-1.europe-west3-c.c.tactical-works-297321.internal>
2021-02-04 21:40:50 +0000

    all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - maine function
+*Return: 0
+*/
+int main(void)
+{
+	unsigned int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
