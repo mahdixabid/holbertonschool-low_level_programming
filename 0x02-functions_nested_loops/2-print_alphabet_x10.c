925d710482163fec9e923c0c7a8228026bf88b37
mahdixabid <mahdiabid158@gmail.com>
2021-02-09 11:02:19 +0100

    alphabet10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,23 @@ char w;
 for (w = 'a'; w <= 'z'; ++w)
 {
 _putchar(w);
+_putchar('\n');
 _putchar(w);
+_putchar('\n');
 _putchar(w);
+_putchar('\n');
 _putchar(w);
+_putchar('\n');
 _putchar(w);
+_putchar('\n');
 _putchar(w);
+_putchar('\n');
 _putchar(w);
+_putchar('\n');
 _putchar(w);
+_putchar('\n');
 _putchar(w);
+_putchar('\n');
 _putchar(w);
 
 }
