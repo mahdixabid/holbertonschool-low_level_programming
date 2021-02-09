50e1f8829286fd2d1feb61fdd6c677da8aac210d
mahdixabid <mahdiabid158@gmail.com>
2021-02-09 11:37:44 +0100

    alphax10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
+
 /**
-* print_alphabet - function to print alphabet in lowercase.
-*
-* Return:0  (sucess)
-*/
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void print_alphabet_x10(void)
 {
 char w;
 int i;
-for( i = 1; i <= 10; ++i)
+for (i = 1; i <= 10; ++i)
 {
-for( w = 'a' ; w <= 'z'; ++w)
+for (w = 'a' ; w <= 'z'; ++w)
 {
 _putchar(w);
 }
