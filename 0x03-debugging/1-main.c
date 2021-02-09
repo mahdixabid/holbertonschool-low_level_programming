d59b83f60035ede56ea06fdf62d3cba182dc11ab
mahdixabid <yuri@instance-1.europe-west3-c.c.tactical-works-297321.internal>
2021-02-09 20:45:19 +0000

    just a comment that's all :/

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+        printf(":(\n");
+        i = 0;
+/*        while (i < 10)
+  *      {
+   *    putchar(i);
+    *    }
+     */
+        printf("\\o/\n");
+        return (0);
+}
