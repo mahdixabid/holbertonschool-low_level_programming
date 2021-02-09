2390c74234db0083e003149a58f9fb6cc9d9e21f
mahdixabid <yuri@instance-1.europe-west3-c.c.tactical-works-297321.internal>
2021-02-09 22:14:10 +0000

    find the biggest number debugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * largest_number - returns the largest 3 numbers
+ * @a: integer
+ * @b: integer
+ * @c: integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+if (a > b && a > c)
+{
+return (a);
+}
+else if (b > a && b > c)
+{
+return (b);
+}
+else if (c > a && c > b)
+{
+return (c);
+}
+}
