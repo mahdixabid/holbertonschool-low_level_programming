6d7bf94312a3cc31a21e028cc37d4241470b8cd1
mahdixabid <yuri@instance-1.europe-west3-c.c.tactical-works-297321.internal>
2021-02-09 22:20:48 +0000

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,14 +7,18 @@
 
 int main(void)
 {
-        int i;
-        printf(":(\n");
-        i = 0;
-/*        while (i < 10)
-  *      {
-   *    putchar(i);
-    *    }
-     */
-        printf("\\o/\n");
-        return (0);
+		int i;
+
+		printf("Infinite loop incoming :(\n");
+
+		i = 0;
+	/*
+	*	while (i < 10)
+	*	{
+	*			putchar(i);
+	*	}
+	*/
+		printf("Infinite loop avoided! \\o/\n");
+
+		return (0);
 }
