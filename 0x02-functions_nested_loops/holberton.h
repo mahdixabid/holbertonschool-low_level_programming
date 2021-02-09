2c364d67d8b0470ce42bf6032ca83e549dc44e0f
mahdixabid <mahdiabid158@gmail.com>
2021-02-09 10:34:50 +0100

    holberton

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 int _putchar(char c);
-=======
-int _putchar(char c)
->>>>>>> 32ad6e2388047af811648453df805aa1f02f80cc
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+int _abs(int x);
+int print_last_digit(int x);
 void jack_bauer(void);
-void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
+void times_table(void);
 void print_times_table(int n);
