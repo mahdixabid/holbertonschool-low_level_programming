947fbe19019371312cc70d4b5a98cea04a571551
mahdixabid <mahdiabid158@gmail.com>
2021-02-09 10:26:42 +0100

    holberton

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,53 +1,13 @@
-<<<<<<< HEAD
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-=======
-/** @redteam1337 **/
+#include <unistd.h>
 
-/** _putchar **/
-int _putchar(char c);
-
-/** print_alphabet **/
-void print_alphabet(void);
-
-/** print_alphabet_x10 **/
-void print_alphabet_x10(void);
-
-/**  _islower **/
-int _islower(int c);
-
-/** isalpha **/
-int _isalpha(int c);
-
-/** print_sign **/
-int print_sign(int n);
-
-/** int_abs **/
-int _abs(int);
-
-/** print_last_digit **/
-int print_last_digit(int);
-
-/** jack_bauer **/
-void jack_bauer(void);
-
-/** times_table **/
-void times_table(void);
-
-/** add **/
-int add(int, int);
-
-/** print_to_98 **/
-void print_to_98(int n);
->>>>>>> 32ad6e2388047af811648453df805aa1f02f80cc
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
