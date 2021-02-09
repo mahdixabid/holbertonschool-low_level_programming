300ad2390319ab60bf41663098cb21e2ff14dce0
mahdixabid <yuri@instance-1.europe-west3-c.c.tactical-works-297321.internal>
2021-02-09 09:24:04 +0000

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include"holberton.h"
+
+/**
+*main - main function
+*Return: 0
+*/
+int main(void)
+{ 
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('\n');
+return (0);
+}
