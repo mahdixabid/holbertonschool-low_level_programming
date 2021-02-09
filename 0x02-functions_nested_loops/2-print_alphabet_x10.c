57fa3fda03421f5901ba072f48a2b5608aa36f3a
mahdixabid <mahdiabid158@gmail.com>
2021-02-09 11:00:48 +0100

    alphabet10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* print_alphabet - function to print alphabet in lowercase.
+*
+* Return:0  (sucess)
+*/
+void print_alphabet(void)
+{
+char w;
+
+for (w = 'a'; w <= 'z'; ++w)
+{
+_putchar(w);
+_putchar(w);
+_putchar(w);
+_putchar(w);
+_putchar(w);
+_putchar(w);
+_putchar(w);
+_putchar(w);
+_putchar(w);
+_putchar(w);
+
+}
+_putchar('\n');
+}
