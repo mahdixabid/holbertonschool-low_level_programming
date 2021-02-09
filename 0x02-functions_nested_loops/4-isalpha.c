b74bec28e8956723df6704db20af22b10e58edf4
mahdixabid <mahdiabid158@gmail.com>
2021-02-09 13:19:18 +0100

    lowercase or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_islower- lowercase
+*@c: integer
+* Return: sometime 0 or 1
+*/
+int _isalpha(int c);
+{
+if (((c >= 'a' && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
