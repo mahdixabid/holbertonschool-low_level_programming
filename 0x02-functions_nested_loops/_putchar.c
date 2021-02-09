04bb347c5a8793e3a218e4bc0c95efa82962f745
mahdixabid <mahdiabid158@gmail.com>
2021-02-09 10:13:17 +0100

    holberton

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,37 @@
+/** @redteam1337 **/
+
+/** _putchar **/
+int _putchar(char c);
+
+/** print_alphabet **/
+void print_alphabet(void);
+
+/** print_alphabet_x10 **/
+void print_alphabet_x10(void);
+
+/**  _islower **/
+int _islower(int c);
+
+/** isalpha **/
+int _isalpha(int c);
+
+/** print_sign **/
+int print_sign(int n);
+
+/** int_abs **/
+int _abs(int);
+
+/** print_last_digit **/
+int print_last_digit(int);
+
+/** jack_bauer **/
+void jack_bauer(void);
+
+/** times_table **/
+void times_table(void);
+
+/** add **/
+int add(int, int);
+
+/** print_to_98 **/
+void print_to_98(int n);
