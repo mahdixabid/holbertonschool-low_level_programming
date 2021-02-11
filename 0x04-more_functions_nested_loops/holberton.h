71608ddebb651f0cc92f7b3997cc1ef1b71b98dd
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-11 23:07:43 +0100

    Update holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,15 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <stdio.h>
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-#endif /* HOLBERTON_H */
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif
