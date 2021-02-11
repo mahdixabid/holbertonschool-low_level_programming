9a6cb456cc9729791b4b99bafca69046b1a89286
mahdixabid <yuri@instance-1.europe-west3-c.c.tactical-works-297321.internal>
2021-02-11 21:57:59 +0000

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - print most numbers
+ *
+ * Return;
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
