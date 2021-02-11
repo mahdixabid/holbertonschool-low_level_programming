0d40719470c61d34b0b14ed4bb416412a100fc5a
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-11 23:11:35 +0100

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
 /**
-* print_numbers - prints numbers from 0 to 9.
+* print_most_numbers - prints numbers from 0 to 9 excluding 2 and 4.
 *
 * Return: output.
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-int i = 0;
+int i;
 
-while (i < 10)
+for (i = 0; i < 10; ++i)
+{
+if (i != 2 && i != 4)
 {
 _putchar(i + '0');
-i++;
+}
 }
 _putchar('\n');
 }
