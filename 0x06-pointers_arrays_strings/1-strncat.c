a2192ece4fd4da8f7782dc510d1f6037fd90016a
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-18 13:17:32 -0800

    function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * _strncat - two strings
+ * @dest: input of string 1
+ * @src: input of string 2
+ * @n: times
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != 0 && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
