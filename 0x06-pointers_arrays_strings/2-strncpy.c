94a661c98e6e1ec7cdc808c16eb77aff7d7d54bb
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-18 22:28:06 +0100

    compares two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-
-	i = 0;
-	while (i < n && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
-	i = i;
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+i = i;
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
