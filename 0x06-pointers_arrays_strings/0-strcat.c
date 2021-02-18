89e34cd3355c79872e4859091f445014070de866
mahdixabid <mahdiabid158@gmail.com>
2021-02-18 14:43:30 +0100

    two stingers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,26 @@
 #include "holberton.h"
 /**
- * _strcat - function that concatenates 2 strings
- * @s1: first string
- * @s2: second string
- * @i: number of strings of s1
- * @n: number of strings of s2
+ * _strcat -function that concatenates 2 strings
+ * @dest: first string
+ * @src: second string
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 int i;
 int n;
-i =0;
+i = 0;
 while (dest[i] != '\0')
-	 {
-		 i++;
-	 }
- n=0;
- while(src[n] !='\0')
-	 {
-		 n++;
-	 }
- dest[i]= src [n];
- n++;
- i++;
- dest[i] = '0';
- return (dest);
+{
+i++;
+}
+n = 0;
+while (src[n] != '\0')
+{
+dest[i] = src[n];
+i++;
+n++;
+}
+dest[i] = '\0';
+return (dest);
 }
