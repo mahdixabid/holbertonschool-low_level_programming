d781f7ef990ddd5b66d3b90c722937a7aff96890
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-18 22:29:37 +0100

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,23 +9,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-
-	i = 0;
-
-	while (dest[i] != 0)
-	{
-		i++;
-	}
-
-	j = 0;
-
-	while (src[j] != 0 && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	return (dest);
+int i;
+int j;
+i = 0;
+while (dest[i] != 0)
+{
+i++;
+}
+j = 0;
+while (src[j] != 0 && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+return (dest);
 }
