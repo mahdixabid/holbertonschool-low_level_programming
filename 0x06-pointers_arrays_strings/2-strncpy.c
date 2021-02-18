bf093d2d512cc9a628a41a7a99773c6d78041306
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-18 13:25:25 -0800

    compares two strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strncpy - a copy of string
+ * @dest:the returns
+ * @src: pointer
+ * @n: config
+ *
+ * Return: the parameter dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	i = i;
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
