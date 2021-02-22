1e457fb43b60d506b428ac214ea49776d197522a
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-22 13:28:52 -0800

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,17 +4,16 @@
  * @a: The character to print
  *
  */
-void print_chessboard(char (*a)[8])
+void	print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+int	i = 0, j;
 
-	for (i = 0; i < 8; i++)
-		{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-			_putchar('\n');
-		}
+	while (i < 8)
+	{
+	j = 0;
+	while (j < 8)
+	_putchar(a[i][j++]);
+	_putchar('\n');
+	i++;
+	}
 }
