093cdc9e0f2c0f68424d79823d4b977438f371d1
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-22 14:17:43 -0800

    files config

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
