333f3ae42a1dcecc7730f94c6a46db674e44c959
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-02-25 23:10:50 +0100

    function that returns 1 if a string is a palindrome and 0 if not

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * is_palindrome - find a palindrome string
+ * @s: string for check
+ *
+ * Return: 1 if is palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (*s == i)
+		return (0);
+	return (1);
+}
