32e93948e606a9502333d3f71bdb583075edf2f1
mahdixabid <yuri@instance-1.europe-west3-c.c.tactical-works-297321.internal>
2021-02-28 22:54:19 +0000

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+*main - adds two positive integers.
+*@argc: integer.
+*@argv: string.
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i, j, x;
+
+i = 1;
+x = 0;
+	while (i < argc)
+	{
+	j = 0;
+		while (j < argv[i][j])
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+			printf("Error\n");
+			return (1);
+			}
+		j++;
+		}
+	x += atoi(argv[i]);
+	i++;
+	}
+printf("%d\n", x);
+return (0);
+}
