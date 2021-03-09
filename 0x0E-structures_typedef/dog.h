d4d3b18a959ef338c2307a52bd5e9624bef1c652
mahdixabid <mahdixabid>
2021-03-09 20:55:36 +0100

    dogi

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ *struct dog - print describe dog
+ *@name: char
+ *@age:float
+ *@owner:char
+ */
+typedef struct dog {
+    char *name;
+    float age;
+    char *owner;
+
+}dogi;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+struct dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(struct dog_t *d);
+int _putchar(char c);
+#endif
