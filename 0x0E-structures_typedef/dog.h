8c14f8ea087404d228a79cca2f37f6c19d544504
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-03-09 23:20:30 +0100

    dogi

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ typedef struct dog
 } dogi;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-struct dog_t *new_dog(char *name, float age, char *owner);
+struct dogi *new_dog(char *name, float age, char *owner);
 void free_dog(struct dogi *d);
 int _putchar(char c);
 #endif
