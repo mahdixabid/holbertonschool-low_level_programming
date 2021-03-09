fc76cba08dc99e82e0a8c65803a1c324279861fa
mahdixabid <77060124+mahdixabid@users.noreply.github.com>
2021-03-09 23:21:16 +0100

    dogi

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,19 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 /**
- *struct dog - dog is dog
+ *struct dog - print describe dog
  *@name: char
- *@age: float
- *@owner: char
+ *@age:float
+ *@owner:char
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
-} dogi;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-struct dogi *new_dog(char *name, float age, char *owner);
-void free_dog(struct dogi *d);
-int _putchar(char c);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog( dog_t *d);
 #endif
