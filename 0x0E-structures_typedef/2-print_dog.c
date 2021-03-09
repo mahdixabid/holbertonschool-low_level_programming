e8f9576e30fb71aa3de2f22de130febbb80b6de4
mahdixabid <mahdixabid>
2021-03-09 23:06:04 +0100

    print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *_dog - initialize a variable
+ *@name: char
+ *@age: float
+ *@owner: char
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+		printf("Age: %.6f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
+
+
+
+
+
+}
