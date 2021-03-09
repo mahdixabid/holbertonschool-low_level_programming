8d0efc9fbb2a478b353702ee317210afa6e5429e
mahdixabid <mahdixabid>
2021-03-09 23:04:38 +0100

    frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+*free_dog - free dog
+*@d:pointer
+*Return:void
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
+
