24c77395ff979e3e7ee0381480bc152c2b4a250a
mahdixabid <mahdixabid>
2021-03-10 23:29:22 +0100

    searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ *int_index - print the index of the array
+ *@array:int
+ *@size:size if arrays
+ * @array: array of elements
+ *
+ * Return: return the index of the first element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+	for (index = 0 ; index < size ; index++)
+	{
+		if ((*cmp)(array[index]) != 0)
+		{
+			return (index);
+		}
+	}
+	return (-1);
+}
