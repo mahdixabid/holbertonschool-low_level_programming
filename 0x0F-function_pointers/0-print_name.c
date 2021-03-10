86bcf52297ae4fc76f401b8461962820e42b0b05
mahdixabid <mahdixabid>
2021-03-10 23:01:59 +0100

    code fix v4

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,9 +6,11 @@
  * @f: pointer to function
  * Return: function to pointer to print name
  */
-
+ 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL && name != NULL)
-	f(name);
+	if (f != NULL)
+	{
+		(*f)(name);
+	}
 }
