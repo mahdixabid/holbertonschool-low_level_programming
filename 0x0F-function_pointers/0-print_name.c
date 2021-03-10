0b5091cab99aef3584629b74d6f1182b5d8799ff
mahdixabid <mahdixabid>
2021-03-10 22:33:45 +0100

    print a name using fuction pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,6 @@
  * print_name - print a name
  * @name: the string to print
  * @f: pointer to function
- *
  * Return: function to pointer to print name
  */
 void print_name(char *name, void (*f)(char *))
