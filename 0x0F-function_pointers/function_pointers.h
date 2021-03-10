09c3563e96f373aac3e8aa483a15214320c4cc43
mahdixabid <mahdixabid>
2021-03-10 23:05:11 +0100

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
+#include "function_pointers.h"
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
