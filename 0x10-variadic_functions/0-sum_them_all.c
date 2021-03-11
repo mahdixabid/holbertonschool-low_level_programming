f68edcb49a613bf31414b4b7b84c2cf3bb87784b
mahdixabid <mahdixabid>
2021-03-11 23:01:02 +0100

    returns the sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - the sum of all its pa
+ * @n: number of pa
+ *
+ * Return: 0 if n is 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int add = 0, number;
+
+	va_list pa;
+
+	va_start(pa, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		number = va_arg(pa, int);
+		add = add + number;
+	}
+	va_end(pa);
+	return (add);
+}
