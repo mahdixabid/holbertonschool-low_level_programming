46d42ef5b508b9c556cf7c5c9e0b45ce04d1d14e
mahdixabid <mahdixabid>
2021-03-21 19:22:09 +0100

    changed description and fix betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,32 +3,30 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ *print_list - print a list
+ *@h:pointer of type list_t;
+ *Return: count
  */
-size_t print_list(const list_t *h) {
-    int n = 0;
-    if (h==NULL)
-    {
-        return 0;
-    }
-    while (h != NULL)
-    {
-        if (h -> str == NULL)
-        {
-            printf("[0] (nill)\n");
-        }
-        else 
-        {
-            printf ("[%d] %s \n", h->len, h-> str);
-        }
-        n++;
-        h= h-> next;
-    }
+size_t print_list(const list_t *h)
+{
+	int n = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nill)\n");
+		}
+		else
+		{
+			printf("[%d] %s \n", h->len, h->str);
+		}
+		n++;
+		h = h->next;
+	}
 return (n);
 }
