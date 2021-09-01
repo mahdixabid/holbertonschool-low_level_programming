a79cc38e6568c7463faa07212511b41564ee2004
92546518Mahdi@@ <mahdiabid158@gmail.com>
2021-09-01 19:32:58 +0100

    fix code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,8 +13,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 	int value_index;
-		if (array == NULL)
-		return (-1);
+	if (array == NULL)
+	return (-1);
 	while (i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
